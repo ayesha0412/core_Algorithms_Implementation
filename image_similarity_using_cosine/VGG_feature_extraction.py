580faa2922c29e8510bd17d5ccf1729cc62d13c3
ayesha0412 <ayeshatahir0412@gmail.com>
2025-09-11 22:57:10 -0700

    Feature_extraction_using_VGG

--- image_similarity_using_cosine/VGG_feature_extraction.py
+++ image_similarity_using_cosine/VGG_feature_extraction.py
@@ -13,3 +13,35 @@
 
 import numpy as np
 from numpy import linalg as LA
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
+
+
+#understanding the model
+# model=VGG16(weights="imagenet",
+#             input_shape=(224,224,3),
+#             pooling='max',
+#             include_top=False)
+# model.summary()
+
+class VGGNet:
+    def __init__(self):
+        self.input_shape=(224,224,3),
+        self.weights="imagenet",
+        self.pooling='max',
+        self.model=VGG16(weights=self.weights,
+                         input_shape=(self.input_shape[0],self.input_shape[1],self.input_shape[2]),
+                         pooling=self.pooling,
+                         include_top=False
+                         )
+        
+'''usee VGG16 to extract features from an image'''
+def extract_features(self,img_path):
+    img=image.load_img(img_path,target_size=(self.input_shape[0],self.input_shape[1]))
+    img=image.img_to_array(img)
+    img=np.expand_dims(img,axis=0)
+    img=preprocess_input(img)
+    feat=self.model.predict(img)
+    norm_feat=feat[0]/LA.norm(feat[0])
+    return norm_feat
